7452a1d0aaf76e668f438604877f5e8a3c16d4fd
Jake Dibble <jdibble@bsu.edu>
2021-06-02 13:32:35 -0400

    Delete unused component, modify index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css'; 
+import 'bootstrap/dist/js/bootstrap';
+import 'popper.js/dist/popper';
+import 'jquery/dist/jquery';
 import Navbar from './components/navbar';
 import BackgroundImage from './components/background_image';
 
