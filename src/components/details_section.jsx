82886770de5e04d3aee8af1906e8491541bf08d4
Jake Dibble <jdibble@bsu.edu>
2021-06-02 16:44:27 -0400

    Add language icons in skills section

--- src/components/details_section.jsx
+++ src/components/details_section.jsx
@@ -3,6 +3,10 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import phpIcon from './../files/icons/php_icon.jpg';  //90x74
+import jsIcon from './../files/icons/javascript_icon.jpg';
+import mysqlIcon from './../files/icons/mysql_icon.jpg';
 
 class DetailsSection extends Component {
     styles = {
@@ -16,9 +20,26 @@ class DetailsSection extends Component {
                     <Col className="text-center m-4">
                     <Card style={this.style}>
                         <Card.Title>Skills</Card.Title>
-                        <Card.Text>
-                            This will be a list of skills that I have
-                        </Card.Text>
+                        <Container>
+                            <Row className="m-3">
+                                <Col>
+                                    <Image src={phpIcon} />
+                                </Col>
+                                <Col>
+                                    <Image src={jsIcon}/>
+                                </Col>
+                            </Row>
+                            <Row className="m-3">
+                                <Col>
+                                    <Image src={mysqlIcon}/>
+                                </Col>
+                                <Col>Icon</Col>
+                            </Row>
+                            <Row className="m-3">
+                                <Col>Icon</Col>
+                                <Col>Icon</Col>
+                            </Row>
+                        </Container>
                     </Card>
                     </Col>
                     <Col className="text-center m-4">
