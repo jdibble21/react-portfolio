c7a41f00c84a62cb676f071b058352164f55240d
Jake Dibble <jdibble@bsu.edu>
2021-06-02 12:17:59 -0400

    Refactor images to be in same component

--- src/components/background_image.jsx
+++ src/components/background_image.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Image from 'react-bootstrap/Image';
-import picture from './../files/background_image.jpg';
+import backImage from './../files/background_image.jpg';
+import profileImage from './../files/profile_image.jpg';
 
 class BackgroundImage extends Component {
     render() { 
         return (
-            <Image src={picture} alt="background image" fluid />
+            <div>
+                <Image src={backImage} alt="background image" fluid />
+                <Image className="mx-auto d-block" src={profileImage} alt="profile image" roundedCircle fluid/>
+            </div>
         );
     }
 }
