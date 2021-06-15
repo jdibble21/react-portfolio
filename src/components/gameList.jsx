f1351495e9ab4418c617ad10761707d1df90c7eb
Jake Dibble <jdibble@bsu.edu>
2021-06-15 10:03:31 -0400

    Add forest runner icon, edit gamelist formatting

--- src/components/gameList.jsx
+++ src/components/gameList.jsx
@@ -5,35 +5,43 @@ import Col from 'react-bootstrap/Col';
 import { Card } from 'react-bootstrap';
 import SO_Icon from './../files/icons/space_odyssey.png';
 import SI_Icon from './../files/icons/spooky_invaders.png';
+import FR_Icon from './../files/icons/forest_run_icon.png';
 import Image from 'react-bootstrap/Image';
+
 class GamesList extends Component {
-    state = {  }
     render() { 
         return ( 
             <Container>
                 <Row>
                     <Col>
-                        <Card className="m-5">
+                        <Card className="m-4">
                             <Card.Title>Space Odyssey</Card.Title>
                             <div class="img-square-wrapper"><Image src={SO_Icon}></Image></div>
-                            <Card.Body>Top-down scrolling space shooter. Shoot enemy ships and collect powerups through two levels with a unique boss at the end of each!</Card.Body>
+                            <Card.Subtitle className="m-4">Development Duration: 5 weeks</Card.Subtitle>
+                            <Card.Body>Top-down scrolling space shooter. Shoot enemy ships and collect powerups through two levels with a unique boss at the end of each! Created with inspriation from Galaga.</Card.Body>
+                            <Card.Link className="m-2" href="https://bsu-cs315.github.io/FP-Space-Odyssey/" target="_blank">Play!</Card.Link>
                         </Card>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <Card className="m-5">
+                        <Card className="m-4">
                             <Card.Title>Spooky Invaders</Card.Title>
                             <div class="img-square-wrapper"><Image src={SI_Icon}></Image></div>
-                            <Card.Body>Arcade inspired built with inspiration from space invaders and galaga. Shoot down the enemy skulls as you dodge their bone missles! Theme for game was "Spooky Quarantine", created within 1 week. Fun fact: All the visuals you see were done from scratch by me!</Card.Body>
+                            <Card.Subtitle className="m-4">Development Duration: 1 week</Card.Subtitle>
+                            <Card.Body>Arcade inspired built with inspiration from Space Invaders. Shoot down the enemy skulls as you dodge their bone missles! Theme for game was "Spooky Quarantine", created within 1 week. Fun fact: All the visuals you see were done from scratch by me!</Card.Body>
+                            <Card.Link className="m-2" href="https://bsu-cs315.github.io/FP-Space-Odyssey/" target="_blank">Play!</Card.Link>
                         </Card>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                        <Card className="m-5">
+                        <Card className="m-4">
                             <Card.Title>Forest Runner</Card.Title>
-                            <Card.Body>Side scrolling platformer style game. Collect the sword, defeat the bandit, and make it to the end to win!</Card.Body>
+                            <div class="img-square-wrapper"><Image src={FR_Icon}></Image></div>
+                            <Card.Subtitle className="m-4">Development Duration: 2 weeks</Card.Subtitle>
+                            <Card.Body>Side scrolling platformer style game, a sort of hybrid between Mario and the Legend of Zelda. Collect the sword, defeat the bandit, and make it to the end to win!</Card.Body>
+                            <Card.Link className="m-2" href="https://bsu-cs315.github.io/FP-Space-Odyssey/" target="_blank">Play!</Card.Link>
                         </Card>
                     </Col>
                 </Row>
